e4172c9536b6dc9ed7249eab955bb05baef76c22
Rugnar <rugnarlodbrok@gmail.com>
2019-01-10 02:58:54 +0300

    scatter movement and scaling

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
-from collections import Counter
 from itertools import chain
 
 import numpy as np
 from kivy.app import App
-from kivy.graphics.texture import Texture
-from kivy.graphics.vertex_instructions import Rectangle
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.transformation import Matrix
 from kivy.config import Config
-from kivy.uix.label import Label
+from kivy.graphics.texture import Texture
+from kivy.graphics.transformation import Matrix
+from kivy.graphics.vertex_instructions import Rectangle
 from kivy.uix.scatter import Scatter
 
 Config.set('graphics', 'width', '600')
@@ -43,7 +40,6 @@ def to_bmp(m):
     a = np.min(m)
     b = np.max(m)
     r = ((m - a) * 255 / (b - a + 1)).astype(np.uint8)
-    # print(Counter(r.reshape(64 * 64)))
     return np.moveaxis(np.stack([r, r, r]), 0, -1)
 
 
@@ -55,19 +51,14 @@ def transform_vector(m, v):
 
 
 class MandelbrotScene:
-    def __init__(self, center=(0, 0), r=1., rez=256, depth=64):
-        self.matrix = Matrix().translate(*center, 0).scale(1 / r, 1 / r, 1)
-        # self.center = np.array(center, dtype=np.float64)
-        # self.r = r
+    def __init__(self, rez=256, depth=64):
+        self.matrix = Matrix().translate(-1.5, -1, 0).scale(2, 2, 1)
         self.rez = rez
         self.depth = depth
 
     def get_data(self):
-        # x, y = self.center
-        # r = self.r
-        p0 = transform_vector(self.matrix, (-1, -1))
+        p0 = transform_vector(self.matrix, (0, 0))
         p1 = transform_vector(self.matrix, (1, 1))
-        print("p0, p1:", p0, p1)
         _, _, arr = mandelbrot_set(*p0, *p1, self.rez, self.rez, self.depth)
         return arr
 
@@ -85,10 +76,9 @@ class MyScatter(Scatter):
         with self.canvas.before:
             s = 600
             self.rect = Rectangle(size=(s, s), pos=self.pos, texture=self.get_texture())
-        self.view_matrix = Matrix().scale(s/2, s/2, 1).translate(-1, -1, 0)  # (0, 300) on canvas is mapped to (-1, 1) on scene
+        self.view_matrix = Matrix().scale(s, s, 1).translate(0, 0, 0)
 
     def get_texture(self):
-        print('get_texture')
         arr = self.mandelbrot_scene.get_data()
 
         self.my_texture.blit_buffer(to_bmp(arr).tobytes(), bufferfmt='ubyte', colorfmt='rgb')
@@ -102,19 +92,21 @@ class MyScatter(Scatter):
 
     def on_touch_up(self, touch):
         if touch.grab_current is not None:
-            print('up!', self.pos, self.scale,)
+            print('up!', self.pos, self.scale, )
         r = super().on_touch_up(touch)
         if not self._touches:
             m = self.mandelbrot_scene
             V = self.view_matrix
             T = self.transform
             # M = V @ T @ V.inv() @ M
-            m.matrix = m.matrix.multiply(V.inverse()).multiply(T.inverse()).multiply(V)
+            print(T)
+            m.matrix = m.matrix.multiply(V.inverse().multiply(T.inverse()).multiply(V))
             self.rect.texture = self.get_texture()
             self.transform = Matrix()  # self.transform.identity() works strangely
         return r
 
     def on_double_tap(self, touch):
+        # TODO: this is broken now
         pos = np.array(touch.pos)
         m = self.mandelbrot_scene
         c = (pos - np.array(self.rect.pos)) / np.array(self.rect.size) - .5
@@ -138,8 +130,6 @@ def on_resize(*args):
 def main():
     Window.bind(on_resize=on_resize)
     return FucktrallApp().run()
-    # m = np.array(Matrix().get()).reshape((4, 4))
-    # print(m)
 
 
 if __name__ == '__main__':
